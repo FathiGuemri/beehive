831a8ca5f447ec9fbbe37fb37bee40b2c042cee5
FathiGuemri <zlatan.guemri@gmail.com>
2021-10-16 22:18:57 +0100

    configuration core lib

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,7 @@
 
 import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
-
+import { ConfigService } from "@nestjs/config";
 import { AppModule } from './app/app.module'
 
 async function bootstrap() {
@@ -13,8 +13,10 @@ async function bootstrap() {
   const globalPrefix = 'api'
   app.setGlobalPrefix(globalPrefix)
   const port = process.env.PORT || 3333
+  const config = app.get(ConfigService);
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix)
+    Logger.log(`Running in ${config.get("environment")} mode`);
   })
 }
 
